d2153a57716d0acdcd8d7d9b91e196521ef3e9e1
iliakhlyzov <khlyzoff@gmail.com>
2021-02-10 19:54:48 +0300

    added new game prima

---
+++ src/games/brain-prime.js
@@ -0,0 +1,20 @@
+import logic from '../index.js';
+import getRandom from '../getRandom.js';
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  if (num <= 1) return false;
+  const maxDivisor = num / 2;
+  for (let i = 2; i < maxDivisor; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const data = () => {
+  const number = getRandom(0, 100);
+  console.log(`Question: ${number}`);
+  return isPrime(number) ? 'yes' : 'no';
+};
+export default () => logic(gameDescription, data);
