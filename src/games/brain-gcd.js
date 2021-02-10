d44a6946b693a26bd2ab6f1893c2271d07c56b5f
iliakhlyzov <khlyzoff@gmail.com>
2021-02-10 16:04:37 +0300

    updated

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,10 +1,17 @@
 import getRandom from '../getRandom';
+import logic from '../../src/index.js';
+
 
 const findGcd = (a, b) => (b === 0 ? a : findGcd(b, a % b));
 
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 
-const a = getRandom();
-const b = getRandom();
-const gcd = findGcd(a, b);
+const data = () => {
+  const number1 = getRandom();
+  const number2 = getRandom();
+  const gcd = findGcd(number1, number2);
+  console.log(`Question: ${number2} ${number1}`)
+  return gcd;
+}
 
+export default () => logic(gameDescription, data);
