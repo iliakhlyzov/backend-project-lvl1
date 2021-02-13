473e3c2f2f41d7f5b6d8925d5ccd4d18aae64f90
iliakhlyzov <khlyzoff@gmail.com>
2021-02-13 10:35:08 +0300

    updated

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,30 +1,5 @@
 #!/usr/bin/env node
-/* eslint-disable no-unused-vars */
 
-import readlineSync from 'readline-sync';
-import getRandom from '../src/getRandom.js';
+import runGame from '../src/games/brain-even.js'
 
-const isEven = (num) => num % 2 === 0;
-const getBoolean = (answer) => {
-  if (answer === 'yes' || answer === 'no') return answer === 'yes';
-  return null;
-};
-
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-let countRight = 0;
-while (countRight !== 3) {
-  const number = getRandom(0, 10);
-  console.log(`Question: ${number}`);
-  const answer = readlineSync.question('Your answer: ');
-  if (getBoolean(answer) === isEven(number)) {
-    console.log('Correct!');
-    countRight += 1;
-  } else {
-    countRight = 0;
-    console.log(`Let's try again, ${name}!`);
-  }
-}
-console.log(`Congratulations, ${name}!`);
+runGame();
