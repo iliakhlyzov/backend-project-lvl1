ea2becbdf0cf0ab27ad6541eae2edaca003c43ff
iliakhlyzov <khlyzoff@gmail.com>
2021-02-13 12:18:00 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 // архитектура приложения
 // этот файл взаимодействует с пользователем, приинимает от него данные,
-// а также с логикой игры, принимает от нее правильный ответ
+// а также логику игры, принимает правильный ответ
 // сравнивает решение и ответ
 import readlineSync from 'readline-sync';
 
-export default (gameDescription, startGame) => {
+export default (gameDescription, startGame, roundCount = 3) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(gameDescription);
-  const winsToEnd = 3;
-  let wins = 0;
-  while (wins !== winsToEnd) {
+  let isWin = true;
+  let round = 1;
+  do {
     const [solution, question] = startGame();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     if (solution === answer) {
       console.log('Correct!');
-      wins += 1;
+      round += 1;
     } else {
       console.log(`Let's try again, ${name}!`);
-      wins = 0;
+      isWin = false;
+      break;
     }
+  } while (round <= roundCount);
+  if (isWin) {
+    console.log(`Congratulations, ${name}!`);
   }
-  console.log(`Congratulations, ${name}!`);
 };
