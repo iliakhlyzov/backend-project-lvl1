89973c0409e5d0fb0002d38b9deef7794730e994
iliakhlyzov <khlyzoff@gmail.com>
2021-02-13 10:45:11 +0300

    updated

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -2,17 +2,13 @@ import getRandom from '../getRandom.js';
 import startGame from '../index.js';
 
 const isEven = (num) => num % 2 === 0;
-const getBoolean = (answer) => {
-  if (answer === 'yes' || answer === 'no') return answer === 'yes';
-  return null;
-};
 
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getGame = () => {
   const number = getRandom(0, 10);
   console.log(`Question: ${number}`);
-  return getBoolean(isEven(number));
+  return isEven(number) ? 'yes' : 'no';
 };
 
 export default () => startGame(gameDescription, getGame);
